49a168be446ab2477db48a5c3c0ddf5ff50fcbb5
rowlavel <rowlavel@iu.edu>
2020-04-16 14:34:10 -0400

    fucking model sucked

--- src/cnn.py
+++ src/cnn.py
@@ -31,16 +31,14 @@ class CNN:
         # create the sequential model
         model = keras.Sequential([
             keras.layers.Conv2D(50, (3, 3), input_shape=(28, 28, 1), activation='relu'),
-            keras.layers.Dropout(0.25),
             keras.layers.Conv2D(100, (3, 3), activation='relu'),
-            keras.layers.Dropout(0.25),
-            keras.layers.Conv2D(150, (3, 3), activation='relu'),
+            keras.layers.MaxPool2D((2,2)),
             keras.layers.Dropout(0.25),
             keras.layers.Conv2D(200, (3, 3), activation='relu'),
+            keras.layers.MaxPool2D((2, 2)),
             keras.layers.Dropout(0.25),
-            keras.layers.Conv2D(350, (3, 3), activation='relu'),
-            keras.layers.Dropout(0.25),
-            keras.layers.Conv2D(500, (3, 3), activation='relu'),
+            keras.layers.Conv2D(400, (3, 3), activation='relu'),
+            keras.layers.MaxPool2D((2, 2)),
             keras.layers.Dropout(0.25),
             keras.layers.Flatten(input_shape=(28,28)),
             keras.layers.Dense(784, activation='relu'),
@@ -90,10 +88,10 @@ if __name__ == "__main__":
         letter_pred = cnn.predict(letter_img_pred)
         imshow(letter_img_disp, letter_pred + 1)
 
-    cnn = CNN(load=True)
+    cnn = CNN(load=False)
     cnn.load_data()
-    #cnn.train()
-    #print("DONE TRAINING")
+    cnn.train()
+    print("DONE TRAINING")
     cnn.test()
 
     #show_image(cnn, 20000)
