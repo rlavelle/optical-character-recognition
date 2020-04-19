018fc2123e2a1f98c4abaa4e3d30ac2e4e946c4a
rowlavel <rowlavel@iu.edu>
2020-04-19 01:30:07 -0400

    new model attempt

--- src/cnn.py
+++ src/cnn.py
@@ -35,9 +35,11 @@ class CNN:
             keras.layers.MaxPool2D((2,2)),
             keras.layers.Dropout(0.25),
             keras.layers.Conv2D(200, (3, 3), activation='relu'),
+            keras.layers.Conv2D(250, (3, 3), activation='relu'),
             keras.layers.MaxPool2D((2, 2)),
             keras.layers.Dropout(0.25),
             keras.layers.Conv2D(400, (3, 3), activation='relu'),
+            keras.layers.Conv2D(512, (3, 3), activation='relu'),
             keras.layers.MaxPool2D((2, 2)),
             keras.layers.Dropout(0.25),
             keras.layers.Flatten(input_shape=(28,28)),
@@ -88,10 +90,10 @@ if __name__ == "__main__":
         letter_pred = cnn.predict(letter_img_pred)
         imshow(letter_img_disp, letter_pred + 1)
 
-    cnn = CNN(load=True)
+    cnn = CNN(load=False)
     cnn.load_data()
-    #cnn.train()
-    #print("DONE TRAINING")
+    cnn.train()
+    print("DONE TRAINING")
     cnn.test()
 
     #show_image(cnn, 20000)
